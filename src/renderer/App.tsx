c026cabbbb163e749f6ca451ea6cf9b7c4d53baf
Dakrain <lhdkhoa11th@gmail.com>
2024-12-17 09:52:07 +0700

    add agora web

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -2,19 +2,26 @@ import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from '@/store/configureStore';
 import { LoadingOverlay } from '@/shared/components/LoadingOverlay/LoadingOverlay';
+import { useState } from 'react';
+import AgoraRTC, { AgoraRTCProvider } from 'agora-rtc-react';
 import Home from './features/Home/Home';
 import Login from './features/Auth/pages/Login/Login';
 
 export default function App() {
+  const [engine] = useState(() =>
+    AgoraRTC.createClient({ mode: 'rtc', codec: 'vp8' }),
+  );
   return (
     <Provider store={store}>
-      <LoadingOverlay />
-      <Router>
-        <Routes>
-          <Route path="/home" element={<Home />} />
-          <Route path="/" element={<Login />} />
-        </Routes>
-      </Router>
+      <AgoraRTCProvider client={engine}>
+        <LoadingOverlay />
+        <Router>
+          <Routes>
+            <Route path="/home" element={<Home />} />
+            <Route path="/" element={<Login />} />
+          </Routes>
+        </Router>
+      </AgoraRTCProvider>
     </Provider>
   );
 }
